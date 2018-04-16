54bdce8041e81642415401e5aea946fe48aa3109
olumide farinmade <rich4millenium@gmail.com>
2018-04-15 21:08:21 -0500

    i've made a necessary changes

--- liri.js
+++ liri.js
@@ -1,139 +1,149 @@
+// DEPENDENCIES
+// =====================================
+// Read and set environment variables
 require("dotenv").config();
 
-var fs = require("fs");
-var Spotify = require("node-spotify-api");
+// Import the Twitter NPM package.
 var Twitter = require("twitter");
-var request = require("request");
+
+// Import the node-spotify-api NPM package.
+var Spotify = require("node-spotify-api");
+
+// Import the API keys
 var keys = require("./keys");
 
-//Setting global variables
+// Import the request npm package.
+var request = require("request");
 
-// empty string to store song and movie search input
-var searchItem = "";
-// captures the song and search input that gets pushed to the above string
-var userInput = process.argv;
-// used in switch case to capture user command
-var command = process.argv[2];
+// Import the FS package for read/write.
+var fs = require("fs");
 
-//Switch case to trigger different functions depending on user command
+// Initialize the spotify API client using our client id and secret
+var spotify = new Spotify(keys.spotify);
 
-switch (command) {
+// FUNCTIONS
+// =====================================
 
-    case "movie-this":
-        movieInfo();
-        break;
+// Writes to the log.txt file
+var getArtistNames = function(artist) {
+  return artist.name;
+};
 
-    case "my-tweets":
-        getMyTweets();
-        break;
+// Function for running a Spotify search
+var getMeSpotify = function(songName) {
+  if (songName === undefined) {
+    songName = "What's my age again";
+  }
 
-    case "spotify-this-song":
-        spotifySongInfo();
-        break;
+  spotify.search(
+    {
+      type: "track",
+      query: songName
+    },
+    function(err, data) {
+      if (err) {
+        console.log("Error occurred: " + err);
+        return;
+      }
 
-    case "do-what-it-says":
-        doWhatItSays();
-        break;
+      var songs = data.tracks.items;
 
-}
-
-//Get movie info function
-
-function movieInfo() {
-    // captures the movie title the user inputs
-    for (var i = 3; i < userInput.length; i++) {
-        searchItem += userInput[i] + "+";
+      for (var i = 0; i < songs.length; i++) {
+        console.log(i);
+        console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+        console.log("song name: " + songs[i].name);
+        console.log("preview song: " + songs[i].preview_url);
+        console.log("album: " + songs[i].album.name);
+        console.log("-----------------------------------");
+      }
     }
-    // if the user does not input any movie title, it will default to searching for Mr. Nobody
-    if (searchItem === "") {
-        searchItem = "Mr.+Nobody";
+  );
+};
+
+// Function for running a Twitter Search
+var getMyTweets = function() {
+  var client = new Twitter(keys.twitter);
+
+  var params = {
+    screen_name: "cnn"
+  };
+  client.get("statuses/user_timeline", params, function(error, tweets, response) {
+    if (!error) {
+      for (var i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].created_at);
+        console.log("");
+        console.log(tweets[i].text);
+      }
     }
-    // the users movie title input is placed into the search URL
-    var queryURLIMDB = "http://www.omdbapi.com/?t=" + searchItem + "&y=&plot=short&apikey=trilogy";
-    // a request is sent to IMDB for it to send us the movie information back
-    request(queryURLIMDB, function (error, response, body) {
-        // if the movie information comes back succesfully, we display the below information
-        if (!error && response.statusCode === 200) {
+  });
+};
 
-            var responseIMDBObject = JSON.parse(body);
-            console.log("Movie Title: " + responseIMDBObject.Title);
-            console.log("Year: " + responseIMDBObject.Year);
-            console.log("Rated: " + responseIMDBObject.Rated);
-            console.log("Rotten Tomatoes: " + responseIMDBObject.Ratings[1].Value);
-            console.log("Made In: " + responseIMDBObject.Country);
-            console.log("Language: " + responseIMDBObject.Language);
-            console.log("Plot: " + responseIMDBObject.Plot);
-            console.log("Cast: " + responseIMDBObject.Actors);
-        } else {
-            console.log("I am sorry that is not a movie!");
-        }
-    })
-}
+// Function for running a Movie Search
+var getMeMovie = function(movieName) {
+  if (movieName === undefined) {
+    movieName = "Mr Nobody";
+  }
 
-//Get tweets function
+  var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
 
-function getMyTweets() {
-    // link twitter key and token information
-    var twitterKey = new Twitter(keys.twitter);
-    // make request to twitter for my tweet information
-    twitterKey.get("statuses/user_timeline", "petersona78", function (error, tweets, response) {
-        // if the information comes back without any errors 
-        if (!error && response.statusCode === 200) {
-            // this runs if my profile has 20 or more tweets and displays 20 tweets
-            if (tweets.length > 20) {
-                for (var i = 0; i < 20; i++) {
-                    console.log("\n" + tweets[i].text);
-                    console.log("Tweeted at: " + tweets[i].created_at + "\n");
-                }
-                // this runs if my profile has less than 20 tweets and displays all of the tweets I have
-            } else {
-                for (var i = 0; i < tweets.length; i++) {
-                    console.log("\n" + tweets[i].text);
-                    console.log("Tweeted at: " + tweets[i].created_at + "\n");
-                }
-            }
-            // If we do not get any response back from twitter this runs
-        } else {
-            console.log("I am sorry we could not find any tweets!");
-            console.log(error);
-        }
-    });
-}
+  request(urlHit, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var jsonData = JSON.parse(body);
 
-
-//Spotify search function
- 
-function spotifySongInfo() {
-    // link up spotify ID and secret information
-    var spotifyKey = new Spotify(keys.spotify);
-    // captures song title input
-    for (var i = 3; i < userInput.length; i++) {
-        searchItem += userInput[i] + " ";
+      console.log("Title: " + jsonData.Title);
+      console.log("Year: " + jsonData.Year);
+      console.log("Rated: " + jsonData.Rated);
+      console.log("IMDB Rating: " + jsonData.imdbRating);
+      console.log("Country: " + jsonData.Country);
+      console.log("Language: " + jsonData.Language);
+      console.log("Plot: " + jsonData.Plot);
+      console.log("Actors: " + jsonData.Actors);
+      console.log("Rotton Tomatoes Rating: " + jsonData.Ratings[1].Value);
     }
-    // inputs song title into function and sends a request to spotify for information
-    spotifyKey.search({ type: 'track', query: searchItem, limit: 1 }, function (err, data) {
-        // if there is an error log the error  
-        if (err) {
-            console.log(err);
-        }
-        // displays information about the song the user input
-        console.log("\nArtist: " + data.tracks.items[0].artists[0].name);
-        console.log("\nSong Name: " + data.tracks.items[0].name);
-        console.log("\nPreview Link: " + data.tracks.items[0].external_urls.spotify);
-        console.log("\nAlbum: " + data.tracks.items[0].album.name);
-    });
-}
+  });
+};
 
-//Do what it says Function
+// Function for running a command based on text file
+var doWhatItSays = function() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    console.log(data);
 
-function doWhatItSays() {
-    // search in my computers file storage for my random.txt file and capture the text
-    fs.readFile("./random.txt", "utf8", function (err, data) {
-        // split the text information we recieved from random.txt into and array sub strings 
-        var splitString = data.split(",");
-        // set searchItem global variable to the song name we captured from the above variable
-        searchItem = splitString[1];
-        // feed this song name into the spotifyThisSong() function
-        spotifySongInfo();
-    })
-}
+    var dataArr = data.split(",");
+
+    if (dataArr.length === 2) {
+      pick(dataArr[0], dataArr[1]);
+    }
+    else if (dataArr.length === 1) {
+      pick(dataArr[0]);
+    }
+  });
+};
+
+// Function for determining which command is executed
+var pick = function(caseData, functionData) {
+  switch (caseData) {
+  case "my-tweets":
+    getMyTweets();
+    break;
+  case "spotify-this-song":
+    getMeSpotify(functionData);
+    break;
+  case "movie-this":
+    getMeMovie(functionData);
+    break;
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+  default:
+    console.log("LIRI doesn't know that");
+  }
+};
+
+// Function which takes in command line arguments and executes correct function accordigly
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
+};
+
+// MAIN PROCESS
+// =====================================
+runThis(process.argv[2], process.argv[3]);
